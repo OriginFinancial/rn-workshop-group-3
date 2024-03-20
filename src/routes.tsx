2d1e46fad7556e7908530e00b73f3c8bccdc8ad7
Murillo de Miranda Pereira <murillomir0@gmail.com>
2024-03-20 15:36:52 -0300

    feat: create pokedex

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "./screens/home";
+import Pokedex from "./screens/pokedex";
+import { RootStackParamList } from "types";
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function Routes() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Pokedex" component={Pokedex} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/pokedex.tsx
+++ src/screens/pokedex.tsx
@@ -1,14 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "types";
 
-function Home({ navigation }: NativeStackScreenProps<RootStackParamList>) {
+function Pokedex() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Button title="Pokedex" onPress={() => navigation.navigate("Pokedex")} />
+      <Text>Pokedex</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -23,4 +22,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Home;
+export default Pokedex;
